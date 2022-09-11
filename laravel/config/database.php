ad2044ab4f7296e6f60a70254aff72fc83d369b7
kudo <kuasehujikolp@yahoo.co.jp>
2022-09-12 03:55:40 +0900

    dist

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -47,7 +47,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => '3306',
             'database' => env('DB_DATABASE', 'forge'),
             'username' => 'root',
             'password' => 'root_password',
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return \File::get(public_path() . '/index.html');
 });
+
+Route::get('/aaa', [IndexController::class, 'index']);
+
+
